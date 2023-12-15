b010b9ece87c8c0bc32edb7b3da656464ca449f2
sayeef-rahman <sayeef.cse@gmail.com>
2023-12-15 22:02:03 +0600

    navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 "use client";
-import "@/styles/globals.css";
+import "@/styles/main.css";
+import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
